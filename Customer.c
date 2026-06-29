90c8796d6e5d738064131f2ea159064763a8a4cf
SheikhUbair <sheikhubair30@gmail.com>
2026-06-29 03:33:36 -0700

    Addition of customers

---
+++ Customer.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+struct Customer
+{
+    char cust_name[50];
+    int cust_ID;
+    char cust_Address[200];
+    int cust_Room;
+    int cust_NoOfPerson;
+    char cust_Purpose[10];
+    char cust_arrivalDATE[12];
+};
+
+
+int main()
+{
+
+}
